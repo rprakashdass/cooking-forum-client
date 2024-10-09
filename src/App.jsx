04911c08a2aee2de100829c7a6d5934da150e812
nivash77 <151871666+nivash77@users.noreply.github.com>
2024-10-09 09:41:16 +0530

    modified the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Home from './components/Home';
 import Search from './components/Search';
 import Contact from './components/Contact';
 import CommunityPost from './components/CommunityPost';
+import Cart from './products/Cart';
+import { Login } from './components/Login';
+import { SignUp } from './components/SignUp';
+import CartIndex from './products/CartIndex';
 
 function App() {
   return (
@@ -15,7 +19,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/search" element={<Search />} />
-        <Route path="/products" element={<CartIndex />} />
+        <Route path="/CartIndex" element={<CartIndex />} />
         <Route path="/CommunityPost" element={<CommunityPost />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp/>} />
