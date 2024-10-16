e6d414fff56c8e0d1314ca1c5b033b41eefb965c
Yuvan krishna P <143686903+useru1k@users.noreply.github.com>
2024-10-16 23:01:37 +0530

    Update NavBar with add logo and glow the text

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,46 +1,46 @@
 import { Link } from "react-router-dom";
-import Search from "./Search";
-import Home from "./Home";
-import Contact from "./Contact";
-import CommunityPost from "./CommunityPost";
-import { UserContext } from "../util/context/UserContext";
-import { useContext } from "react";
 
 function NavBar() {
-  const { username } = useContext(UserContext);
-    return (
-      <nav className="flex justify-between items-center p-6 bg-gray-100 shadow-md">
-        <div className="text-2xl font-bold">Toppings {username}</div>
-        {/* <h1>Welcome, {username ? username : "Guest"} </h1> */}
-        <ul className="flex gap-6">
-        {/* <li className="hover:underline"> */}
-            <Link to="/Home" className="hover:underline">
-              Home
-            </Link>
-          {/* </li> */}
-          {/* <li className="hover:underline"> */}
-            <Link to="/Search" className="hover:underline">
-            Search
-            </Link>
-          {/* </li> */}
-          {/* <li className="hover:underline"> */}
-            <Link to="/Contact" className="hover:underline">
-              Contact
-            </Link>
-            <Link to="/CartIndex" className="hover:underline">
-              Cart
-            </Link>
-          {/* </li> */}
-          <Link to="/CommunityPost" className="hover:underline">
-              Discuss
-            </Link>
-            <Link to="/login" className="hover:underline">
-              Login
-            </Link>
-        </ul>
-      </nav>
-    );
-  }
-  
-  export default NavBar;
-  
+  return (
+    <nav className="flex justify-between items-center p-6 h-[12vh] bg-gray-200 shadow-md rounded-md">
+      <div className="flex justify-start gap-4">
+        <img
+          src="../src/assets/img/diet.png"
+          alt="Logo"
+          className="w-10 h-10 transform hover:scale-105 transition duration-300"
+        />
+        <div className="text-2xl font-bold text-slate-900 bg-gradient-to-r items-center from-blue-500 via-teal-500 to-pink-500 bg-clip-text text-transparent text-center select-auto transform hover:scale-105 transition duration-300">
+          Toppings
+        </div>
+      </div>
+      <ul className="flex gap-6">
+        <Link
+          to="/Home"
+          className="transform hover:scale-105 transition duration-300 hover:text-gray-500"
+        >
+          Home
+        </Link>
+        <Link
+          to="/Search"
+          className="transform hover:scale-105 transition duration-300 hover:text-gray-500"
+        >
+          Search
+        </Link>
+        <Link
+          to="/Contact"
+          className="transform hover:scale-105 transition duration-300 hover:text-gray-500"
+        >
+          Contact
+        </Link>
+        <Link
+          to="/CommunityPost"
+          className="transform hover:scale-105 transition duration-300 hover:text-gray-500"
+        >
+          Discuss
+        </Link>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
