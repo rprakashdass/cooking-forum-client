3575f7ca0ea58016601e8f888ce5f2f065ea1393
nivash77 <151871666+nivash77@users.noreply.github.com>
2024-11-30 09:14:18 +0530

    Add files via upload

--- src/util/service/api.js
+++ src/util/service/api.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
 
-const API = "https://cooking-forum-server.onrender.com"; 
+
+const API = "http://localhost:7777"; 
 const getDishes = () => axios.get(`${API}/dishes/all`);
-const addDish = (dishData) => axios.post(`${API}/dishes/add`,{dishData}, { withCredentials: true });
-const getRecipesForDish = (id) => axios.get(`${API}/dishes/${id}/recipes`);
-const addRecipeToDish = (dishName, recipeData) => axios.post(`${API}/dishes/${dishName}/recipes/add`,{ recipeData}, { withCredentials: true });
-const addPost=(Post)=>axios.post(`${API}/post/add`,{Post}, { withCredentials: true });
+const addDish = (dishData) => axios.post(`${API}/dishes/add`,dishData);
+// const addDish = (dishData) => axios.post(`${API}/dishes/add`,{dishData}, { withCredentials: true });
+const getRecipesForDish = (dishName) => axios.get(`${API}/dishes/${dishName}/recipes`);
+const addRecipeToDish = (dishName, recipeData) => axios.post(`${API}/dishes/${dishName}/recipes/add`, recipeData);
+const addPost=(Post)=>axios.post(`${API}/post/add`,Post);
 const getPost=(user)=>axios.get(`${API}/post/user/${user}`);
 const getPostAll=()=>axios.get(`${API}/post/all`);
-
+const deleteDish=(dishName)=>axios.delete(`${API}/delete/${dishName}`);
+const getorderProduct=()=>axios.get(`${API}/orderproduct/all`);
+const addUser=(user)=>axios.post(`${API}/users/add`,user);
+const getUser=(username)=>axios.get(`${API}/users/${username}`);
 export {
     getDishes,
     addDish,
@@ -16,5 +21,9 @@ export {
     addRecipeToDish,
     addPost,
     getPost,
-    getPostAll
+    getPostAll,
+    deleteDish,
+    getorderProduct,
+    addUser,
+    getUser
 };
